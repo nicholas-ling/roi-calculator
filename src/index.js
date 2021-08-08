7c42fc2c801fddde6cb3b8a4cf859f2002494dea
Nicholas <7038146+nicholas-ling@users.noreply.github.com>
2021-08-08 19:31:03 -0400

    add google ads

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const html = (
+  <html>
+    <head>
+      <title>condo investment roi calculator</title>
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9765601109946250"
+        crossorigin="anonymous"
+      ></script>
+    </head>
+    <body>
+      <App />
+    </body>
+  </html>
+);
+
+ReactDOM.render(html, document.getElementById("root"));
